f545c4ed7e3648dc5f3d8cd7fa27be500c324ed5
Nathas <nathas_ice@yahoo.com>
2019-12-20 16:30:10 +0800

    Change to limo

---
+++ prac_06/used_cars.py
@@ -0,0 +1,17 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from prac_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    limo = Car("Limousine", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
+    print(limo)
+
+
+main()
