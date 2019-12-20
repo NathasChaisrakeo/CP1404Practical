d1f44504e28a61d15e6d64ab4f32319a5be6e299
Nathas <nathas_ice@yahoo.com>
2019-12-20 19:04:24 +0800

    Fixing underscore line

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,16 +1,19 @@
 class ProgrammingLanguage:
 
     def __init__(self, name, typing, reflection, year):
+        """Constriuctor that takes in parameters and create object"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Determines if a language is dynamically or statically typed"""
         if self.typing == "Dynamic":
             return True
 
     def __str__(self):
+        """Return the details of the object in string format"""
         return "{}, {} Typing, Reflection= {}, First appeared in {}".format(
             self.name, self.typing, self.reflection, self.year)
 
