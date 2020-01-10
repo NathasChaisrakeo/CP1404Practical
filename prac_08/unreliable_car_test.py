ec65e301627574db8707a75ab53287a39325bbfe
Nathas <nathas_ice@yahoo.com>
2020-01-10 18:28:42 +0800

    Committed Good & Bad Car

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -4,9 +4,9 @@ from prac_08.unreliable_car import UnreliableCar
 def main():
 
     good_car = UnreliableCar("Good Car", 100, 90)
-    bad_car = UnreliableCar("Bad Car", 100, 10)
+    bad_car = UnreliableCar("Bad Car", 100, 40)
 
-    for i in range(1, 30):
+    for i in range(1, 15):
         print("Attempting to drive {}km: ".format(i))
         print("{:10} drove {:2}km".format(good_car.name, good_car.drive(i)))
         print("{:10} drove {:2}km".format(bad_car.name, bad_car.drive(i)))
